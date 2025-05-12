044c14ab8ff2da2b39f54436d0fd0531179d00a9
PederGintal <pngintal@stud.ntnu.no>
2025-05-13 00:52:58 +0200

    Fix (maybe)

--- routes/products.js
+++ routes/products.js
@@ -5,11 +5,11 @@ const db = require('../db/connection');
 // GET /api/products
 router.get('/', async (req, res) => {
   try {
-    const [rows] = await db.query('SELECT * FROM Product');
+    const [rows] = await db.query('SELECT * FROM product');
     res.json(rows);
   } catch (err) {
     console.error("MySQL error:", err); // <-- log full error
-    res.status(500).json({ error: 'Database error' });
+    res.status(500).json({ error: 'Database error. DJ Khaled' });
   }
 });
 
