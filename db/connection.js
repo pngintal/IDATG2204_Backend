0aeb3ef26ce66d4e1fe89488dd278befde920cf7
PederGintal <pngintal@stud.ntnu.no>
2025-05-13 00:36:28 +0200

    Fix again again again

--- db/connection.js
+++ db/connection.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql2/promise');
 
-const pool = mysql.createPool({
-  host: process.env.MYSQLHOST,
-  user: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
-  port: process.env.MYSQLPORT
+const db = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
 });
 
-module.exports = pool;
+module.exports = db;
