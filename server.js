18731dda37fd8afb3899dfaf43a7e386c0710c7d
PederGintal <pngintal@stud.ntnu.no>
2025-05-13 00:44:01 +0200

    Port fuck idk

--- server.js
+++ server.js
@@ -10,5 +10,5 @@ app.use(express.json());
 app.use('/api/products', require('./routes/products'));
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, "0.0.0.0", () => console.log(`Server running on port ${PORT}`));
  
