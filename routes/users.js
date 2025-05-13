a1018211898d64c1694c77ce6810a44f31c83ace
PederGintal <pngintal@stud.ntnu.no>
2025-05-13 11:46:38 +0200

    Fixing products.js, and hopefully adding user.js

--- routes/users.js
+++ routes/users.js
@@ -5,7 +5,7 @@ const db = require('../db/connection');
 // GET /api/products
 router.get('/', async (req, res) => {
   try {
-    const [rows] = await db.query('SELECT * FROM product');
+    const [rows] = await db.query('SELECT * FROM user');
     res.json(rows);
   } catch (err) {
     console.error("MySQL error:", err.message); // <-- log full error
