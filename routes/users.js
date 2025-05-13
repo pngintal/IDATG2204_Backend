69abfd0641b70dcb659e75dc37636927389900a9
PederGintal <pngintal@stud.ntnu.no>
2025-05-13 11:50:19 +0200

    Adding new route for users.js to server.js

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const db = require('../db/connection');
 
-// GET /api/products
+// GET /api/users
 router.get('/', async (req, res) => {
   try {
     const [rows] = await db.query('SELECT * FROM user');
