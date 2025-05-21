1538664bc1281e61c40424871547c767d59597e5
PederGintal <pngintal@stud.ntnu.no>
2025-05-21 21:57:20 +0200

    big fix

---
+++ routes/userReviews.js
@@ -0,0 +1,15 @@
+// GET /api/reviews/user/:userID — reviews made by 1 user
+router.get('user/:UserId', async (req, res) => {
+  const { UserIDId } = req.params;
+
+  try {
+    const [rows] = await db.query(
+      'SELECT * FROM review WHERE UserID = ?',
+      [UserId]
+    );
+    res.json(rows);
+  } catch (err) {
+    console.error("MySQL error:", err.message);
+    res.status(500).json({ error: 'Database error' });
+  }
+});
