c03abf25ad91135cff23503163cf2ff78a799828
PederGintal <pngintal@stud.ntnu.no>
2025-05-21 13:34:11 +0200

    trying to fix crashes

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const cors = require('cors');
+const session = require('express-session');
 require('dotenv').config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.use(session({
+  secret: 'secret',
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    secure: false,
+    maxAge: 1000 * 60 * 60 * 24
+  }
+}));
 
 // Routes
 app.use('/api/products', require('./routes/products'));
