791500a6296aa4db7094ef925abf80c2f17e9754
PederGintal <pngintal@stud.ntnu.no>
2025-05-21 22:09:57 +0200

    i am dumb

--- routes/userReviews.js
+++ routes/userReviews.js
@@ -1,6 +1,10 @@
-// GET /api/userReviews/:userID — reviews made by 1 user
+const express = require('express');
+const router = express.Router();
+const db = require('../db/connection');
+
+// GET /api/userReviews/:userId — reviews made by 1 user
 router.get('/:UserId', async (req, res) => {
-  const { UserIDId } = req.params;
+  const { UserId } = req.params;
 
   try {
     const [rows] = await db.query(
